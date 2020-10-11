51c346f4507f1163fba3dc0c299e6e067431aa40
konbo <konbo666@tutanota.com>
2020-10-11 21:58:04 +0530

    Test 1

--- ProcessImage/urls.py
+++ ProcessImage/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from test.views import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('processimage', api, name="api")
 ]
--- test/views.py
+++ test/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+from .serializer import ImageLinkSerializer
+import json
 
-# Create your views here.
+@csrf_exempt
+def api(request):
+    if request.method == "POST":
+        # body_unicode = request.body.decode('utf-8')
+        # data = json.loads(body_unicode)
+        data = JSONParser().parse(request)
+        serializer = ImageLinkSerializer(data=data)
+        if serializer.is_valid():
+            return JsonResponse(serializer.data, status=200)
+        return JsonResponse(serializer.errors, status=400)
+
+    return HttpResponse("Error", status=400)
