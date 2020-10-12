87b76757106b5d43afeceea73889c3a1cf94e97a
konbo <konbo666@tutanota.com>
2020-10-13 01:59:21 +0530

    gs

--- test/views.py
+++ test/views.py
@@ -4,9 +4,12 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
 from .serializer import ImageLinkSerializer
 import requests
+import face_recognition
+import PIL.Image
+import numpy as np
+# api_key = '9jOgLRpjTeROSeGfVFmjSNTH4Aj9cE81'
+# api_secret = 'U1Ctbr3CijFS5NfLFJxYX0cRym0RF7m6'
 
-api_key = '9jOgLRpjTeROSeGfVFmjSNTH4Aj9cE81'
-api_secret = 'U1Ctbr3CijFS5NfLFJxYX0cRym0RF7m6'
 @csrf_exempt
 def api(request):
     if request.method == "POST":
@@ -16,10 +19,17 @@ def api(request):
         if serializer.is_valid():
 
             data = serializer.data
-            pload = {'api_key': api_key, 'api_secret': api_secret, 'image_url': data['image_url'], 'return_attributes': 'gender,age'}
-            r = requests.post('https://api-us.faceplusplus.com/facepp/v3/detect', data=pload)
-                        
-            return JsonResponse(r.json(), status=200)
+            # pload = {'api_key': api_key, 'api_secret': api_secret, 'image_url': data['image_url'], 'return_attributes': 'gender,age'}
+            # r = requests.post('https://api-us.faceplusplus.com/facepp/v3/detect', data=pload)
+
+            file = requests.get(data['image_url'], stream=True).raw
+            im = PIL.Image.open(file)
+            image = np.array(im)
+            face_locations = face_recognition.face_locations(image)
+
+            response = {'face_locations': face_locations}
+
+            return JsonResponse(response, status=200)
 
         return JsonResponse(serializer.errors, status=400)
 
